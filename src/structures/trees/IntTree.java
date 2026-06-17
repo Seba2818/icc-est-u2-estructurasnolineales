0174dbbed0c7fa72880dc7099534dc476376ec03
Your Name <you@example.com>
2026-06-17 08:56:43 -0500

    u

---
+++ src/structures/trees/IntTree.java
@@ -0,0 +1,53 @@
+package structures.trees;
+
+import structures.node.Node;
+
+
+public class IntTree {
+    
+    private Node <Integer> root;
+
+    //Constructor
+
+    public IntTree(){
+        this.root = null;
+    }
+
+    public Node<Integer> getRoot() {
+        return root;
+    }
+
+    public void setRoot(Node<Integer> root) {
+        this.root = root;
+    }
+
+    public void setRoot (Integer value){
+        Node<Integer> node = new Node<Integer>(value);
+        this.root = node;
+    }
+
+    public void add (int value){
+        Node<Integer> node = new Node<Integer>(value);
+        root = addRecursivo (root, node);
+
+    }
+
+    public Node<Integer> addRecursivo(
+        Node<Integer> actual, 
+        Node<Integer> nodeInsertar) {
+
+        if(actual == null) 
+            return nodeInsertar;
+
+        if(actual.getValue() > nodeInsertar.getValue()){
+            actual.setLeft(addRecursivo(actual.getLeft(), nodeInsertar));
+        } else {
+            actual.setRight(addRecursivo(actual.getRight(), nodeInsertar));
+                    
+        }
+
+            
+    return actual;
+        
+    }
+}
