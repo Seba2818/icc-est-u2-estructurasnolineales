b4f4eb0213a144ac205ef05cc22b6f53743b2690
Dan51122 <daniel5011ug@gmail.com>
2026-07-01 08:41:48 -0500

    u

--- src/evaluacion/PersonaController.java
+++ src/evaluacion/PersonaController.java
@@ -1,11 +1,25 @@
 package evaluacion;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class PersonaController {
     
     public Set<Persona> filtrarYOrdenar(List<Persona> personas, int edadUmbral){
+        return null;
+
+    }
+
+    public Map<String, Set<String>> agruparPorEdad(List <Persona> personas, int edadUmbral){
+
+        String nombre = "Juan Perez";
+        String[] palabras = nombre.split(" ");
+        String primerNombre = palabras [0];
+        // ["JUAN", "PEREZ"]
+
+        String pNombre = nombre.split(" ")[0];
+        return null;
 
     }
 }
