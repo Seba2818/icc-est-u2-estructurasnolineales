ca51fecde66db5bafbd2560bb698be103e064181
Dan51122 <daniel5011ug@gmail.com>
2026-07-01 08:32:57 -0500

    u

---
+++ src/evaluacion/PersonaController.java
@@ -0,0 +1,11 @@
+package evaluacion;
+
+import java.util.List;
+import java.util.Set;
+
+public class PersonaController {
+    
+    public Set<Persona> filtrarYOrdenar(List<Persona> personas, int edadUmbral){
+
+    }
+}
