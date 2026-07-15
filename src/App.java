aa204f8bf8d1fd364203b35bcde6714df96fe661
Your Name <you@example.com>
2026-07-15 08:32:50 -0500

    u

--- src/App.java
+++ src/App.java
@@ -25,6 +25,27 @@ public class App {
         // runGraph();
         // runSets();
         runGraph2();
+        runFibonaci();
+    }
+
+    private static void runFibonaci() {
+        long startTime = System.nanoTime();
+        int num = 48;
+        long i = getFibonacci(num);
+        long endTime = System.nanoTime();
+        long executionTime = endTime - startTime;
+
+        double milliseconds = (executionTime / 1_000_000.0);
+
+        System.out.println("Finbonaci de " + num + " es " + i + "  en: " + milliseconds + " milliseconds");
+
+    }
+
+    private static long getFibonacci(int i) {
+        if (i <= 1) {
+            return i;
+        }
+        return getFibonacci(i - 1) + getFibonacci(i - 2);
     }
 
     private static void runGraph2() {
